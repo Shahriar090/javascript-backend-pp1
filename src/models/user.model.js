fa520313ad50e6b31aa4c34dbc6f87c3d72ba8f0
Shahriar <shahriarh655@gmail.com>
2024-06-27 21:14:19 +0600

    Implemented the video schema and model, and added bcrypt and JWT functionality to the user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -48,5 +50,42 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  // Checking if the password has been modified or not, and ensuring that the password does not change every time other fields are modified.
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// compering password
+userSchema.methods().isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+// access and refresh token
+userSchema.methods().generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods().generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 // model
-const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
