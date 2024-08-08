9595ed74b9d5f2bcc449f6f185e9829253ae050a
Shahriar <shahriarh655@gmail.com>
2024-08-08 11:22:35 +0600

    Added upload middleware before register user to upload files

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,21 @@
 import express from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const userRouter = express.Router();
 
-userRouter.post("/register-user", registerUser);
+userRouter.post(
+  "/register-user",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export { userRouter };
