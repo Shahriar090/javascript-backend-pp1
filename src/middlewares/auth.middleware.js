698ffa18c1b4082e777c309b0564978509a1c4f0
Shahriar <shahriarh655@gmail.com>
2024-08-13 11:54:44 +0600

    Implemented middleware for verifying user before logout

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model";
+import ApiError from "../utils/apiError";
+import asyncHandler from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+    // decoding token information
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
