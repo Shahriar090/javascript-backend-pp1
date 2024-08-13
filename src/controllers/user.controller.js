72d6487ba8a35d2e63712cb93c6840d395c78f47
Shahriar <shahriarh655@gmail.com>
2024-08-13 12:28:07 +0600

    Fix a minor issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -162,7 +162,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .clearCookie("accessToken", options)
-    .clearCookie("refreshTOken", options)
+    .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User Logged Out"));
 });
 
